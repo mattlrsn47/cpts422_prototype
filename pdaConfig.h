7570e06cc5ebac03d5b962c35b00e947ddf0d6da
sanz1 <jasanz1@yahoo.com>
2020-12-05 06:27:30 -0800

    added setters

--- pdaConfig.h
+++ pdaConfig.h
@@ -9,11 +9,14 @@ class pdaConfig
 private:
     int maximum_number_of_characters;
     int maximum_number_of_transitions;
+
 public:
     pdaConfig();
     ~pdaConfig();
     void load(std::ifstream configuration);
-    void update_maximum_number_of_transitions(int new_setting);//setter for maximum_number_of_transitions
-    void update_maximum_number_of_characters(int new_setting);//setter for maximum_number_of_characters
+    void update_maximum_number_of_transitions(int new_setting); //setter for maximum_number_of_transitions
+    void update_maximum_number_of_characters(int new_setting);  //setter for maximum_number_of_characters
+    int max_number_of_transitions();        //getter for maximum_number_of_transitions
+    int max_number_of_characters();        //getter for maximum_number_of_characters
 };
 #endif
